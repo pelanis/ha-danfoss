fa2b1cd3e0564c8f68cb59a3b4eb3bf57b584ecd
a.pelanis <pelanis@gmail.com>
2026-01-06 06:56:26 +0200

    Reduce memory footprint for constrained HA environments
    - Lower heap from 256m to 128m max, 32m initial
    - Add 64m metaspace limit to cap native memory
    - Use SerialGC (simpler, less overhead for small heaps)
    - Use tiered compilation level 1 (faster startup, less memory)
    - Reduce thread pool sizes from 16+8 to 4+2 (sufficient for this workload)

--- danfoss-addon/run.sh
+++ danfoss-addon/run.sh
@@ -8,4 +8,9 @@ if [[ "$M4_FIX_ENABLED" == "true" ]]; then
   export JAVA_TOOL_OPTIONS="-XX:UseSVE=0"
 fi
 
-java --enable-preview -Xms64m -Xmx256m -jar app.jar
+java --enable-preview \
+  -Xms32m -Xmx128m \
+  -XX:MaxMetaspaceSize=64m \
+  -XX:+UseSerialGC \
+  -XX:+TieredCompilation -XX:TieredStopAtLevel=1 \
+  -jar app.jar
